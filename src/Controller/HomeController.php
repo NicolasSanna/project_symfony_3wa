975950b8c7494bbd7264f0e7971cf7649a5bf912
NicolasSanna <nico13sanna@gmail.com>
2023-04-25 18:56:27 +0200

    envoi de mail avec Mailhog

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,15 +3,47 @@
 namespace App\Controller;
 
 use App\Repository\AnnonceRepository;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Address;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(AnnonceRepository $annonceRepository): Response
+    public function index(AnnonceRepository $annonceRepository, MailerInterface $mailer): Response
     {
+        $email1 = (new Email())
+            ->from('hello@example.com')
+            ->to('you@example.com')
+            //->cc('cc@example.com')
+            //->bcc('bcc@example.com')
+            //->replyTo('fabien@example.com')
+            //->priority(Email::PRIORITY_HIGH)
+            ->subject('Time for Symfony Mailer!')
+            ->text('Sending emails is fun again!')
+            ->html('<p>See Twig integration for better HTML integration!</p>');
+
+        $mailer->send($email1);
+
+        $email2 = (new TemplatedEmail())
+        ->from('fabien@example.com')
+        ->to(new Address('ryan@example.com'))
+        ->subject('Thanks for signing up!')
+
+        // path of the Twig template to render
+        ->htmlTemplate('emails/signup.html.twig')
+
+        // pass variables (name => value) to the template
+        ->context([
+            'expiration_date' => new \DateTime('+7 days'),
+            'username' => 'foo',
+        ]);
+
+        $mailer->send($email2);
         $annonces = $annonceRepository->findBy(['is_visible' => true]);
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
